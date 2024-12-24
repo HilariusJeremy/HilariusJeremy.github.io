ee784d4de83a0fe87d30326543bcc1474588e3be
hilariusjeremy <hilariusjeremy@gmail.com>
2024-12-24 11:46:25 +0700

    Imported image from file

--- src/AboutPage/AboutPage.jsx
+++ src/AboutPage/AboutPage.jsx
@@ -1,12 +1,13 @@
 import './AboutPage.css'
 import NavBar from '../NavBar/NavBar'
+import headshot from '../assets/headshot.png'
 
 const AboutPage = () => {
     return (
         <div className='about'>
             <NavBar />
             
-            <img src="/headshot.png" alt="Description of the image"/>
+            <img src={headshot} alt="Description of the image"/>
             <h1>Hilarius Jeremy Immanuel Litano</h1>
             <p>Computer Science NTU Singapore</p>
         </div>
