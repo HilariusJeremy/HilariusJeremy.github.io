93945e81d08f50bef569518eaec5d2d37ae6a461
Hilarius Jeremy <hilariusjeremy@gmail.com>
2025-01-03 22:42:13 +0700

    fix styling of error component

--- src/ErrorPage/ErrorPage.jsx
+++ src/ErrorPage/ErrorPage.jsx
@@ -15,7 +15,7 @@ const ErrorPage = () => {
                 <h1>Halt!</h1>
                 <h1>You Shall Proceed No Further</h1>
             </div>
-            <div className="error-text">
+            <div className="error-body">
                 <p>The page you are seeking is beyond reach, lost in the abyss of cyberspace.</p>
                 <p>Perhaps it never existed, or maybe it wandered off on an epic quest. </p>
                 <p>Regardless, it is not here, and you must return to the realm from whence you came.</p>
